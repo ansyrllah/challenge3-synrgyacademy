88f81a98f3a68b88cd33aa8f4b88f68ede3326c3
ansyrllah <ansyarullahgo@gmail.com>
2024-04-11 11:15:11 +0700

    selesaiin fungsi filtercarByAvailability

--- scripts/filterCarByAvailability.js
+++ scripts/filterCarByAvailability.js
@@ -6,6 +6,21 @@ function filterCarByAvailability(cars) {
   const result = [];
 
   // Tulis code-mu disini
+  
+  // question: kenapa salah?
+  // cars.forEach(car => {
+  //   if (car.available = true) {
+  //     result.push(car)
+  //     console.log(car);
+  //     console.log(car.manufacture)
+  //   }
+  // });
+
+  for (let index = 0; index < cars.length; index++) {
+    if (cars[index].available == true) {
+      result.push(cars[index])
+    }
+  }
 
   // Rubah code ini dengan array hasil filter berdasarkan availablity
   return result;
